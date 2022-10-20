b076fc45b00af3a448af6ff3c746a6e94ca0f41f
singularitti <singularitti@outlook.com>
2022-10-20 01:53:50 -0400

    Add plotting histograms in scripts/q1.jl

--- scripts/q1.jl
+++ scripts/q1.jl
@@ -1,10 +1,47 @@
 using JackknifeAnalysis
-using Statistics
+using LaTeXStrings
+using Plots
+using Plots.Measures
 
-v1 = readdata("data/v1")
-v2 = readdata("data/v2")
-v3 = readdata("data/v3")
-v4 = readdata("data/v4")
-v5 = readdata("data/v5")
+if !isdir("tex/plots")
+    mkpath("tex/plots")
+end
+
+v1 = read("data/v1", Population)
+v2 = read("data/v2", Population)
+v3 = read("data/v3", Population)
+v4 = read("data/v4", Population)
+v5 = read("data/v5", Population)
 
 map(mean, (v1, v2, v3, v4, v5))
+
+let variables = (v1, v2, v3, v4, v5)
+    for n in (1000, 10000)
+        histograms = []
+        for (i, variable) in enumerate(variables)
+            X = sample(variable, ContinuousSampler(n))
+            means = mean.(X)
+            push!(
+                histograms,
+                histogram(
+                    means;
+                    legend=:none,
+                    framestyle=:box,
+                    xlims=extrema(means),
+                    ylims=(0, Inf),
+                    xlabel=L"$\bar{v}_%$i$",
+                    ylabel=i in (1, 4) ? "frequency" : "",
+                    titlefontsize=5,
+                    labelfontsize=5,
+                    tickfontsize=4,
+                    top_margin=-1mm,
+                    bottom_margin=-1mm,
+                    left_margin=0mm,
+                    right_margin=-1mm,
+                ),
+            )
+            plot(histograms...)
+        end
+        savefig("tex/plots/sample means N=$n.pdf")
+    end
+end
