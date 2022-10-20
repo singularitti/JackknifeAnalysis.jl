d8c64292814ac26f00d3664290191b2bb35fd55c
singularitti <singularitti@outlook.com>
2022-10-20 02:44:59 -0400

    Add `truevar` & `truestd` in src/stats.jl

--- src/stats.jl
+++ src/stats.jl
@@ -1,4 +1,4 @@
-export mean, var, std
+export mean, var, truevar, std, truestd
 
 mean(population::Population) = sum(population) / length(population)
 mean(sample::Sample) = sum(sample) / length(sample)
@@ -12,5 +12,15 @@ function var(sample::Sample)
     return sum(abs2, sample .- μ) / (length(sample) - 1)
 end
 
+function truevar(population::Population, sampler::ContinuousSampler)
+    μ = mean(population)
+    samples = sample(population, sampler)
+    return sum(abs2, mean(s) - μ for s in samples) / (length(samples) - 1)
+end
+
 std(population::Population) = sqrt(var(population))
 std(sample::Sample) = sqrt(var(sample))
+
+function truestd(population::Population, sampler::ContinuousSampler)
+    return sqrt(truevar(population, sampler))
+end
