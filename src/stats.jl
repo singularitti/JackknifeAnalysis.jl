337f0b267dc606673a241bdfa038e6aa82527695
singularitti <singularitti@outlook.com>
2022-10-20 22:46:56 -0400

    Rename `truevar` to `var`, `truestd` to `std`

--- src/stats.jl
+++ src/stats.jl
@@ -1,4 +1,4 @@
-export mean, var, truevar, std, truestd, autocor
+export mean, var, std, autocor
 
 mean(population::Population) = sum(population) / length(population)
 mean(sample::Sample) = sum(sample) / length(sample)
@@ -7,20 +7,18 @@ function var(population::Population)
     μ = mean(population)
     return sum(abs2, population .- μ) / length(population)
 end
-var(sample::Sample) = var(sample, mean(sample))
-var(sample::Sample, mean) = sum(abs2, sample .- mean) / (length(sample) - 1)
-
-function truevar(population::Population, sampler::PartitionSampler)
+function var(population::Population, sampler::PartitionSampler)
     μ = mean(population)
     samples = sample(population, sampler)
     return sum(abs2, mean(s) - μ for s in samples) / (length(samples) - 1)
 end
+var(sample::Sample) = var(sample, mean(sample))
+var(sample::Sample, mean) = sum(abs2, sample .- mean) / (length(sample) - 1)
 
 std(population::Population) = sqrt(var(population))
 std(sample::Sample) = sqrt(var(sample))
-
-function truestd(population::Population, sampler::PartitionSampler)
-    return sqrt(truevar(population, sampler))
+function std(population::Population, sampler::PartitionSampler)
+    return sqrt(var(population, sampler))
 end
 
 function autocor(population::Population, n)
