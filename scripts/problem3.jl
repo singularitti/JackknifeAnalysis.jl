0b69fa541f5553484d7105e6e9d95f67de4458d6
singularitti <singularitti@outlook.com>
2022-12-09 17:11:56 -0500

    Simplify `plot_autocor_time_*` functions to `autocortimeplot`

--- scripts/problem3.jl
+++ scripts/problem3.jl
@@ -1,6 +1,5 @@
 using JackknifeAnalysis
 using Plots
-using Plots.Measures
 
 if !isdir("tex/plots")
     mkpath("tex/plots")
@@ -21,41 +20,14 @@ function guess_nₘₐₓ(sample)
     return findfirst(<(0), Iterators.map(Base.Fix1(autocor, sample), 1:length(sample))) - 1
 end
 
-function plot_autocor_time_u(u::SimpleSample, path="tex/plots/MD_tau_energy.pdf")
-    plot(; legend=:none, right_margin=2mm)
-    xlabel!(raw"$n_\textnormal{cut}$")
-    ylabel!(raw"$\tau$")
-    nₘₐₓ = guess_nₘₐₓ(u)
-    𝛕 = map(Base.Fix1(int_autocor_time, u), 1:nₘₐₓ)
-    plot!(1:nₘₐₓ, 𝛕; label="potential energy")
-    xlims!((1, Inf))
-    ylims!(extrema(𝛕))
-    return savefig(path)
-end
-function plot_autocor_time_t(
-    temperature::SimpleSample, path="tex/plots/MD_tau_temperature.pdf"
-)
-    plot(; legend=:none, right_margin=2mm)
-    xlabel!(raw"$n_\textnormal{cut}$")
-    ylabel!(raw"$\tau$")
-    nₘₐₓ = guess_nₘₐₓ(temperature)
-    𝛕 = map(Base.Fix1(int_autocor_time, temperature), 1:nₘₐₓ)
-    plot!(1:nₘₐₓ, 𝛕)
-    xlims!((1, Inf))
-    ylims!(extrema(𝛕))
-    return savefig(path)
-end
-function plot_autocor_time_v(virial::SimpleSample, path="tex/plots/MD_tau_virial.pdf")
-    plot(; legend=:none, right_margin=2mm)
-    xlabel!(raw"$n_\textnormal{cut}$")
-    ylabel!(raw"$\tau$")
-    nₘₐₓ = guess_nₘₐₓ(virial)
-    𝛕 = map(Base.Fix1(int_autocor_time, virial), 1:nₘₐₓ)
-    plot!(1:nₘₐₓ, 𝛕; label="virial")
-    xlims!((1, Inf))
-    ylims!(extrema(𝛕))
-    return savefig(path)
-end
+autocortimeplot(u, guess_nₘₐₓ(u); label="potential energy")
+savefig("tex/plots/MD_tau_energy.pdf")
+
+autocortimeplot(temperature, guess_nₘₐₓ(temperature); label="temperature")
+savefig("tex/plots/MD_tau_temperature.pdf")
+
+autocortimeplot(virial, guess_nₘₐₓ(virial); label="virial")
+savefig("tex/plots/MD_tau_virial.pdf")
 
 function covariance_matrix(variables)
     m = [cov(variable₁, variable₂) for variable₁ in variables, variable₂ in variables]
