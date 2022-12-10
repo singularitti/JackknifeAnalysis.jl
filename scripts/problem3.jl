d347886ef7d29e44b4a9471913a848166c7102a4
singularitti <singularitti@outlook.com>
2022-12-10 00:15:14 -0500

    Update scripts/problem3.jl

--- scripts/problem3.jl
+++ scripts/problem3.jl
@@ -5,16 +5,16 @@ if !isdir("tex/plots")
     mkpath("tex/plots")
 end
 
-autocorplot(U, 3000; label="potential energy")
+autocorplot(U, 100; label="potential energy")
 autocorplot!(K, 100; label="kinetic energy")
-autocorplot!(U + K, 3000; label="total energy")
+autocorplot!(U + K, 100; label="total energy")
 savefig("tex/plots/MD_autocor_energy.pdf")
 
-autocorplot(temperature, 300)
+autocorplot(temperature, 100; label="temperature")
 savefig("tex/plots/MD_autocor_temperature.pdf")
 
-plot_autocor_v(virial, 300)
-savefig("tex/plots/MD_autocor_virial.pdf")
+# autocorplot(virial, 300; label="virial")
+# savefig("tex/plots/MD_autocor_virial.pdf")
 
 function guess_nₘₐₓ(sample)
     return findfirst(<(0), Iterators.map(Base.Fix1(autocor, sample), 1:length(sample))) - 1
@@ -26,8 +26,8 @@ savefig("tex/plots/MD_tau_energy.pdf")
 autocortimeplot(temperature, guess_nₘₐₓ(temperature); label="temperature")
 savefig("tex/plots/MD_tau_temperature.pdf")
 
-autocortimeplot(virial, guess_nₘₐₓ(virial); label="virial")
-savefig("tex/plots/MD_tau_virial.pdf")
+# autocortimeplot(virial, guess_nₘₐₓ(virial); label="virial")
+# savefig("tex/plots/MD_tau_virial.pdf")
 
 function covariance_matrix(variables)
     m = [cov(variable₁, variable₂) for variable₁ in variables, variable₂ in variables]
